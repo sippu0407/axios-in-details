37ea3be40fa681f59ef8e9896b23e7920fb1f13c
suryabhan singh <suryabhan.singh.0407@gmail.com>
2024-01-12 17:04:20 +0530

    done with code

--- src/index.js
+++ src/index.js
@@ -4,6 +4,17 @@ const app=express();
 
 const axios=require('axios');
 
+//interceptors always on the top
+
+axios.interceptors.request.use(
+    request => {
+     request.headers['clientId']='hbshabxhdwb';
+     return request;
+    },
+    error=> {return error}
+)
+
+
 app.get('/test',()=>{
  
      axios.get('https://jsonplaceholder.typicode.com/todos/1')
@@ -14,6 +25,23 @@ app.get('/test',()=>{
         console.log(error);
      })
 })
+
+//concurrent requests
+// const promise1= axios.get('url');
+// const promise2=axios.get('xyz');
+
+// Promise.all(promise1,promise2).then((responseArray)=>{
+//     const combined={
+//     data1:responseArray[0].data,
+//     data2:responseArray[1].data
+//     }
+//     console.log("combined data");
+// }).catch((e)=>{console.log(e)}).finally(()=>{ console.log("concurrent request")})
+
+
+
+
+
 app.listen(5000,()=>{
     console.log("server started");
 })
